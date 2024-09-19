613911c1ee10791f1191983dc710527265dbe91e
PEACE <peacecsaba@gmail.com>
2024-09-19 08:36:57 +0200

    Disables Forward/Back navigation with mouse side buttons! (Plugin git)

--- src/equicordplugins/2FaOnServers/index.tsx
+++ src/equicordplugins/2FaOnServers/index.tsx
@@ -1,21 +1,27 @@
+/*
+ * Vencord, a Discord client mod
+ * Copyright (c) 2024 Vendicated and contributors
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
 import definePlugin from "@utils/types";
 
 export default definePlugin({
-	name: "2FaOnServers",
-	description: "never get bothered about 2fa moderation again on servers!",
-	authors: [
-		{
-			id: 1003477997728313405n,
-			name: "Death",
-		},
-	],
-	patches: [
-		{
-			find: "APPLICATION_SUBSCRIPTION_EXPIRATION:",
-			replacement: [{
-				match: /GUILD_MFA_WARNING:return/,
-				replace: `GUILD_MFA_WARNING:return;return`
-			}]
-		}
-	]
+    name: "2FaOnServers",
+    description: "never get bothered about 2fa moderation again on servers!",
+    authors: [
+        {
+            id: 317206043039891459n,
+            name: "PEACE",
+        },
+    ],
+    patches: [
+        {
+            find: "APPLICATION_SUBSCRIPTION_EXPIRATION:",
+            replacement: [{
+                match: /GUILD_MFA_WARNING:return/,
+                replace: "GUILD_MFA_WARNING:return;return"
+            }]
+        }
+    ]
 });
