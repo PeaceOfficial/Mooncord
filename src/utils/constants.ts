f1d72df8bfaded0a1c576a4401f4a0ba9b614534
PEACE <peacecsaba@gmail.com>
2024-09-19 23:09:17 +0200

    teszt

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -577,10 +577,6 @@ export const Devs = /* #__PURE__*/ Object.freeze({
         name: "RamziAH",
         id: 1279957227612147747n,
     },
-    peaceofficial: {
-        name: "peaceofficial",
-        id: 317206043039891459n
-    },
 } satisfies Record<string, Dev>);
 
 export const EquicordDevs = Object.freeze({
