de5975f94cae3635de7e284d48c092cfbd932365
PEACE <peacecsaba@gmail.com>
2024-09-24 03:50:49 +0200

    test1234

--- src/components/VencordSettings/VencordTab.tsx
+++ src/components/VencordSettings/VencordTab.tsx
@@ -98,7 +98,7 @@ function MooncordSettings() {
         ];
 
     return (
-        <SettingsTab title="Mooncord Settings123">
+        <SettingsTab title="Mooncord Settings1234">
             <DonateCard image={donateImage} />
             <Forms.FormSection title="Quick Actions">
                 <QuickActionCard>
