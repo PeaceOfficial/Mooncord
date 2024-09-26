23e8c702137984cc959bd621f51b198e1ee78549
PEACE <peacecsaba@gmail.com>
2024-09-26 21:54:22 +0200

    Fix Multiple plugins for latest Discord update, Mooncord 1.1 version released !

--- src/plugins/fakeNitro/index.tsx
+++ src/plugins/fakeNitro/index.tsx
@@ -245,11 +245,11 @@ export default definePlugin({
         },
         // Allow stickers to be sent everywhere
         {
-            find: "canUseCustomStickersEverywhere:function",
+            find: "canUseCustomStickersEverywhere:",
             predicate: () => settings.store.enableStickerBypass,
             replacement: {
-                match: /canUseCustomStickersEverywhere:function\(\i\){/,
-                replace: "$&return true;"
+                match: /(?<=canUseCustomStickersEverywhere:)\i/,
+                replace: "()=>true"
             },
         },
         // Make stickers always available
@@ -263,15 +263,15 @@ export default definePlugin({
         },
         // Allow streaming with high quality
         {
-            find: "canUseHighVideoUploadQuality:function",
+            find: "canUseHighVideoUploadQuality:",
             predicate: () => settings.store.enableStreamQualityBypass,
             replacement: [
                 "canUseHighVideoUploadQuality",
                 "canStreamQuality",
             ].map(func => {
                 return {
-                    match: new RegExp(`${func}:function\\(\\i(?:,\\i)?\\){`, "g"),
-                    replace: "$&return true;"
+                    match: new RegExp(`(?<=${func}:)\\i`, "g"),
+                    replace: "()=>true"
                 };
             })
         },
@@ -286,10 +286,10 @@ export default definePlugin({
         },
         // Allow client themes to be changeable
         {
-            find: "canUseClientThemes:function",
+            find: "canUseClientThemes:",
             replacement: {
-                match: /canUseClientThemes:function\(\i\){/,
-                replace: "$&return true;"
+                match: /(?<=canUseClientThemes:)\i/,
+                replace: "()=>true"
             }
         },
         {
@@ -297,8 +297,8 @@ export default definePlugin({
             replacement: [
                 {
                     // Overwrite incoming connection settings proto with our local settings
-                    match: /CONNECTION_OPEN:function\((\i)\){/,
-                    replace: (m, props) => `${m}$self.handleProtoChange(${props}.userSettingsProto,${props}.user);`
+                    match: /function (\i)\((\i)\){(?=.*CONNECTION_OPEN:\1)/,
+                    replace: (m, funcName, props) => `${m}$self.handleProtoChange(${props}.userSettingsProto,${props}.user);`
                 },
                 {
                     // Overwrite non local proto changes with our local settings
@@ -391,10 +391,10 @@ export default definePlugin({
         },
         // Allow using custom app icons
         {
-            find: "canUsePremiumAppIcons:function",
+            find: "canUsePremiumAppIcons:",
             replacement: {
-                match: /canUsePremiumAppIcons:function\(\i\){/,
-                replace: "$&return true;"
+                match: /(?<=canUsePremiumAppIcons:)\i/,
+                replace: "()=>true"
             }
         },
         // Separate patch for allowing using custom app icons
@@ -415,10 +415,10 @@ export default definePlugin({
         },
         // Allow using custom notification sounds
         {
-            find: "canUseCustomNotificationSounds:function",
+            find: "canUseCustomNotificationSounds:",
             replacement: {
-                match: /canUseCustomNotificationSounds:function\(\i\){/,
-                replace: "$&return true;"
+                match: /(?<=canUseCustomNotificationSounds:)\i/,
+                replace: "()=>true"
             }
         }
     ],
