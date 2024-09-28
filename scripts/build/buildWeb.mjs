f68aa7589b4fa4c326f605077f9a583a1c5a96a1
PEACE <peacecsaba@gmail.com>
2024-09-28 20:11:11 +0200

    Fix Reporter & Update Banners

--- scripts/build/buildWeb.mjs
+++ scripts/build/buildWeb.mjs
@@ -23,7 +23,7 @@ import { appendFile, mkdir, readdir, readFile, rm, writeFile } from "fs/promises
 import { join } from "path";
 import Zip from "zip-local";
 
-import { BUILD_TIMESTAMP, commonOpts, globPlugins, IS_DEV, IS_REPORTER, VERSION } from "./common.mjs";
+import { BUILD_TIMESTAMP, commonOpts, globPlugins, IS_DEV, IS_REPORTER, IS_COMPANION_TEST, VERSION } from "./common.mjs";
 
 /**
  * @type {esbuild.BuildOptions}
