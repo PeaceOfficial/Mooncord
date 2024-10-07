b26d04fca0aca31a640aaad7d0acea239a825f32
PEACE <62814840+PeaceOfficial@users.noreply.github.com>
2024-10-07 10:58:52 +0200

    backup updater.

--- src/components/VencordSettings/UpdaterTab.tsx
+++ src/components/VencordSettings/UpdaterTab.tsx
@@ -159,7 +159,7 @@ function Updatable(props: CommonProps) {
                         if (outdated) {
                             setUpdates(changes);
                         } else {
-                            setUpdates(changes);
+                            setUpdates([]);
                             Toasts.show({
                                 message: "No updates found!",
                                 id: Toasts.genId(),
