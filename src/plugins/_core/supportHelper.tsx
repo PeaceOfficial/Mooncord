baffc0936f92c1c3c5c2b4dfb4bf236572ced268
PEACE <62814840+PeaceOfficial@users.noreply.github.com>
2024-10-10 21:39:05 +0200

    Some Plugins Updates, for latest discord update...

--- src/plugins/_core/supportHelper.tsx
+++ src/plugins/_core/supportHelper.tsx
@@ -156,8 +156,8 @@ export default definePlugin({
     patches: [{
         find: ".BEGINNING_DM.format",
         replacement: {
-            match: /BEGINNING_DM\.format\(\{.+?\}\),(?=.{0,100}userId:(\i\.getRecipientId\(\)))/,
-            replace: "$& $self.ContributorDmWarningCard({ userId: $1 }),"
+            match: /BEGINNING_DM\.format\(\{.+?\}\),(?=.{0,300}(\i)\.isMultiUserDM)/,
+            replace: "$& $self.renderContributorDmWarningCard({ channel: $1 }),"
         }
     }],
 
@@ -261,7 +261,8 @@ export default definePlugin({
         }
     },
 
-    ContributorDmWarningCard: ErrorBoundary.wrap(({ userId }) => {
+    renderContributorDmWarningCard: ErrorBoundary.wrap(({ channel }) => {
+        const userId = channel.getRecipientId();
         if (!isPluginDev(userId) || !isMooncordPluginDev(userId)) return null;
         if (RelationshipStore.isFriend(userId) || isPluginDev(UserStore.getCurrentUser()?.id) || isMooncordPluginDev(UserStore.getCurrentUser()?.id)) return null;
 
